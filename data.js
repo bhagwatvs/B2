7b6aa1a39c01c5d83f7d19d2d1feeebf6293afd0
Vaibhav Bhagwat <vaibhavbhagwat9156089644@gmail.com>
2025-07-06 16:17:19 +0530

    initialized git repo. and added some js code

---
+++ data.js
@@ -0,0 +1,21 @@
+/*let name="vaibhav";
+let age=25;
+let isactive=true;
+let score;
+let empty=null;
+let big=123456789724743890n;
+let unique=Symbol("id");
+
+//console.log(name);*/
+
+{
+    var x=100;
+    console.log(x);
+}
+
+
+if(100>50)
+    console.log("ok");
+
+
+
